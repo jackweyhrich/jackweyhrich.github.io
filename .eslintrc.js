3f8815959d98f4aff384228464d4c7e21264b439
Jack Weyhrich <jackweyhrich@gmail.com>
2022-12-20 22:50:11 -0600

    Properly implement prettier only-warn for js project

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,9 +9,8 @@ module.exports = {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["prettier"],
+  plugins: ["prettier", "only-warn"],
   rules: {
-    // "prettier/prettier": ["error"],
     "require-jsdoc": 0,
     quotes: ["warn", "double"],
   },
