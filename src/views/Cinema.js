2a5ab8e507ed6f82a1d19ddf071dcf53979d1feb
Jack Weyhrich <jackweyhrich@gmail.com>
2025-01-03 23:54:26 -0500

    Add cinema intro and first entry for 2025

--- src/views/Cinema.js
+++ src/views/Cinema.js
@@ -1,5 +1,46 @@
 function Cinema() {
-  return <h1>Cinema Corner</h1>;
+  return (
+    <>
+      <h1>Cinema Corner</h1>
+      <p>
+        For the last several years, I&apos;ve had the idea to keep a log of all
+        the new movies I watch. This year, 2025, I&apos;d like to make good on
+        that idea. I&apos;ll only keep track of the movies that I haven&apos;t
+        seen before, but I&apos;m hoping to continue my resolution from 2024 to
+        focus on watching new movies rather than rewatching ones I&apos;ve seen
+        before. I may still include some of re-watched films here, but I
+        won&apos;t prioritize them.
+      </p>
+
+      <h2>
+        2025-01-03:{" "}
+        <a
+          href="https://en.wikipedia.org/wiki/Nosferatu_(2024_film)"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Nosferatu (2024)
+        </a>
+      </h2>
+      <p>
+        I saw this movie in theaters with my girlfriend and friends - it was a
+        horror film that ended up having some pretty funny moments. With his
+        frankly ridiculous mustache, the Count Orlok character looked a lot like
+        Jim Carrey&apos;s Eggman from Sonic the Hedgehog (whose{" "}
+        <a
+          href="https://en.wikipedia.org/wiki/Sonic_the_Hedgehog_3_(film)"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          third film
+        </a>{" "}
+        is coincidentally in theaters this same month). The dissatisfying
+        ending, otherwise predictable plot and flat characters left me
+        unimpressed with the film, but I had fun and enjoyed lots of laughs with
+        my friends throughout the experience.
+      </p>
+    </>
+  );
 }
 
 export default Cinema;
